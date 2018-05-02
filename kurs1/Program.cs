9789e5577074fe702e47ca25bd0c8a4c5165354d
ztach <ztach@poczta.onet.pl>
2018-05-02 12:44:12 +0200

    Dodaj pliki projektów.

---
+++ kurs1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kurs1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("cześć {0}", args[0]);
+        }
+    }
+}
