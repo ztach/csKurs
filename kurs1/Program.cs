3de3121de155bf99d291deb322bc19e94f1a3213
ztach <ztach@poczta.onet.pl>
2018-05-03 15:11:00 +0200

    refaktoryzacja

--- kurs1/Program.cs
+++ kurs1/Program.cs
@@ -9,33 +9,52 @@ namespace kurs1
         {
             for (; ; )
             {
-                Console.Write("PODAJ IMIĘ: ");
-                string name = Console.ReadLine();
-                Console.WriteLine("cześć " + name);
+                pobierzImie();
+                podajWiek();
 
-                Console.Write("PODAJ WIEK: ");
-
-                bool czyInt = int.TryParse(Console.ReadLine(), out int wiek);
-                if (!czyInt)
+                Console.WriteLine("");
+                Console.WriteLine("");
+                Console.WriteLine("x - EXIT");
+                string x = Console.ReadLine();
+                if (x == "x")
                 {
-                    Console.WriteLine("to nie liczba");
-                }
-                else if (wiek > 17)
-                {
-
-                    Console.WriteLine("masz " + wiek + " lat - idziemy na browara");
-                }
-                else
-                {
-                    Console.WriteLine("masz " + wiek + " lat - idziemy na mleko");
+                    break;
                 }
 
-
-                
-                Console.ReadLine();
-                Console.Clear();
-
+                czyscioch();
             }
+        }
+
+        private static void czyscioch()
+        {
+            Console.Clear();
+        }
+
+        private static void podajWiek()
+        {
+            Console.Write("PODAJ WIEK: ");
+
+            bool czyInt = int.TryParse(Console.ReadLine(), out int wiek);
+            if (!czyInt)
+            {
+                Console.WriteLine("to nie liczba");
+            }
+            else if (wiek > 17)
+            {
+
+                Console.WriteLine("masz " + wiek + " lat - idziemy na browara");
+            }
+            else
+            {
+                Console.WriteLine("masz " + wiek + " lat - idziemy na mleko");
+            }
+        }
+
+        private static void pobierzImie()
+        {
+            Console.Write("PODAJ IMIĘ: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("cześć " + name);
         }
     }
 }
