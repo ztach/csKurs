a1f8eb39f989dbf884be5b1de2594d35d38288db
ztach <ztach@poczta.onet.pl>
2018-05-04 00:47:34 +0200

    programik dziennik ucznia

---
+++ DziennikUcznia/Dziennik.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DziennikUcznia
+{
+    class Dziennik
+    {
+        List<double> oceny = new List<double>();
+
+        public void podajOcene(double ocena)
+        {
+            oceny.Add(ocena);
+        }
+
+        public double sredniaOcen()
+        {
+            double sumaOcen = 0;
+
+            foreach (var o in oceny)
+            {
+                sumaOcen += o;
+            }
+            return sumaOcen / oceny.Count();
+        }
+
+
+    }
+}
---
+++ DziennikUcznia/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DziennikUcznia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dziennik dziennik = new Dziennik();
+            Console.WriteLine("PODAJ OCENĘ");
+
+            for(; ; )
+            {
+                bool t = Double.TryParse(Console.ReadLine(), out double f);
+                if (!t)
+                {
+                    Console.WriteLine("to nie jest liczba: " + f);
+                }
+                else if( f > 0 )
+                {
+                    Console.WriteLine("to jest: " + f);
+                    dziennik.podajOcene(f);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+
+            Console.WriteLine("wynik");
+            Console.WriteLine(dziennik.sredniaOcen());
+            string s = Console.ReadLine();
+
+
+        }
+    }
+}
