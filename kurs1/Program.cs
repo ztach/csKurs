901663892aecdbdf5e8c9396190e8d3e6440bee6
ztach <ztach@poczta.onet.pl>
2018-05-03 14:10:23 +0200

    podaj_wiek

--- kurs1/Program.cs
+++ kurs1/Program.cs
@@ -7,11 +7,30 @@ namespace kurs1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("napisz coś");
-
-            string name = Console.ReadLine() ;
-            
+            Console.Write("PODAJ IMIĘ: ");
+            string name = Console.ReadLine();
             Console.WriteLine("cześć " + name);
+
+            Console.Write("PODAJ WIEK: ");
+            
+            bool czyInt = int.TryParse(Console.ReadLine(), out int wiek);
+            if (!czyInt)
+            {
+                Console.WriteLine("to nie liczba");
+            }
+            else if(wiek > 17)
+            {
+
+                Console.WriteLine("masz " + wiek + " lat - idziemy na browara");
+            }
+            else
+            {
+                Console.WriteLine("masz " + wiek + " lat - idziemy na mleko");
+            }
+            
+
+            Console.Write("ENTER");
+            Console.ReadLine();
         }
     }
 }
